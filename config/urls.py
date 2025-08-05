cbd0911c3ae75b9394d0b575f5d6eb0106ce479c
moritarui <r.morita.sys24@morijyobi.ac.jp>
2025-08-05 14:23:43 +0900

    デプロイのエラーの修正

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('library.urls')),
     path('admin/', admin.site.urls),
     path('library/', include('library.urls')),  
     path('accounts/', include('accounts.urls')),
